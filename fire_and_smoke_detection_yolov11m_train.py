0ae2a4f85c4666b22d9ae32e8ce14132d5f7f46f
vietnguyensaccount <vietnguyensemail@gmail.com>
2025-06-29 06:43:22 +0700

    Update and rename fire_and_smoke_detection_yolov11m_train to fire_and_smoke_detection_yolov11m_train.py

--- fire_and_smoke_detection_yolov11m_train.py
+++ fire_and_smoke_detection_yolov11m_train.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    # 👇 Choose your model config or weights
+    # Choose your model config or weights
     model = YOLO("./yolo11m.pt")  # pretrained medium model
     
     # Train settings
     model.train(
-        data="data.yaml",      # dataset config (paths, nc, names)
+        data="data.yaml",      # dataset config
         imgsz=640,             # training image size
-        batch=8,               # adjust for your GPU memory
+        batch=8,               # adjust for suitable GPU memory
         epochs=100,            # number of epochs
         workers=10,
-        device=0,              # GPU id (use 'cpu' for CPU)
+        device=0,              # GPU id ('cpu' for CPU)
     )
